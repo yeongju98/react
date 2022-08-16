3d5f869c22d1a5a26a6ed5963f965d2ee3cc8425
yeongju <jyj9802@yonsei.ac.kr>
2022-08-17 01:47:04 +0900

    second commit!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movie/:id' element={<Detail />} />
